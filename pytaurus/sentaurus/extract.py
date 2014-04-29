acc9ea434ef6a653bc299a95bcc387b71f08ddc2
Workstation <lunzhy.work@gmail.com>
2014-04-29 10:43:44 +0800

    revise the parsevth method - ENH : calculate the vfb shift of three cells - FIX : fix a small bug in sde template file

--- pytaurus/sentaurus/extract.py
+++ pytaurus/sentaurus/extract.py
@@ -69,43 +69,62 @@ def extractVth(output):
 
 
 def parseVth(prj_path, trip_cells):
+    gate1_start = float(trip_cells.getParam('tc.iso1.width'))
+    gate1_end = gate1_start + float(trip_cells.getParam('tc.gate1.width'))
+    gate2_start = gate1_end + float(trip_cells.getParam('tc.iso2.width'))
+    gate2_end = gate2_start + float(trip_cells.getParam('tc.gate2.width'))
+    gate3_start = gate2_end + float(trip_cells.getParam('tc.iso3.width'))
+    gate3_end = gate3_start + float(trip_cells.getParam('tc.gate3.width'))
+
+    time, vth_gate1 = _calcVfbShift(prj_path, gate1_start, gate1_end, ret_time=True)
+    vth_gate2 = _calcVfbShift(prj_path, gate2_start, gate2_end)
+    vth_gate3 = _calcVfbShift(prj_path, gate3_start, gate3_end)
+    time_vth_list = tuple(zip(time, vth_gate1, vth_gate2, vth_gate3))
+    vth_flatband_file = os.path.join(prj_path, sen.Folder_Miscellaneous, sen.File_Parsed_Vth)
+    with open(vth_flatband_file, 'w') as file:
+        file.write('Time [s]\t\tCell1 [V]\tCell2 [V]\tCell3 [V]\n')
+        for time_vth in time_vth_list:
+            file.write('%.5e\t\t%.5f\t\t%.5f\t\t%.5f\n' % time_vth)
+    return
+
+
+def _calcVfbShift(prj_path, xcoord_start, xcoord_end, ret_time=False):
     # parse effective points
     points_file = os.path.join(prj_path, sen.Folder_Exchange_Data, sen.Points_Location_Subs)
-    channel_main_start = float(trip_cells.getParam('tc.iso1.width')) +\
-                         float(trip_cells.getParam('tc.gate1.width')) + float(trip_cells.getParam('tc.iso2.width'))
-    channel_main_end = channel_main_start + float(trip_cells.getParam('tc.gate2.width'))
-    main_points = []
+    eff_points = []
     with open(points_file) as file:
         info = file.readline()
         for line in file.readlines():
             items = re.split('\s+', line)
             vert_index, xcoord = int(items[0]), float(items[1])
-            if xcoord >= channel_main_start and xcoord <= channel_main_end:
-                main_points.append(vert_index)
+            if xcoord >= xcoord_start and xcoord <= xcoord_end:
+                eff_points.append(vert_index)
+
     vfb_dir = os.path.join(prj_path, sen.Folder_Substrate)
     time_vth_list = []
     for file in os.listdir(vfb_dir):
         if sen.Charge_File_Prefix in file:
             file_path = os.path.join(vfb_dir, file)
             sim_time = tools.parseSimTime(file_path)
-            with open(file_path) as file:
+            with open(file_path, 'r') as file:
                 info = file.readline()
                 voltage_list = []
                 for line in file.readlines():
                     items = re.split('\s+', line)
                     vert_left, vert_right, voltage = int(items[0]), int(items[1]), float(items[2])
-                    if vert_left in main_points and vert_right in main_points:
+                    if vert_left in eff_points and vert_right in eff_points:
                         voltage_list.append(voltage)
-            effective_voltage = sum(voltage_list) / len(voltage_list)
+            effective_voltage = sum(voltage_list) / len(voltage_list)  # all grids have same length
             time_vth_list.append((sim_time, effective_voltage))
-
     # organise time voltage list
     time_vth_list = sorted(time_vth_list, key=lambda x: x[0])
-    vth_flatband_file = os.path.join(prj_path, sen.Folder_Miscellaneous, sen.File_Parsed_Vth)
-    with open(vth_flatband_file, 'w') as file:
-        for time_vth in time_vth_list:
-            file.write('%.5e\t\t%s\n' % time_vth)
-    return
+    time_list = [ tup[0] for tup in time_vth_list]
+    vth_list = [ tup[1] for tup in time_vth_list]
+    if ret_time is True:
+        return time_list, vth_list
+    else:
+        return vth_list
+    return None
 
 
 def test():
