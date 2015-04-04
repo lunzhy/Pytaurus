1a5fae7bd43c29a7f3f8396099bb5f4a6f9df0d9
Workstation <lunzhy.work@gmail.com>
2015-04-04 11:21:32 +0800

    minor revision

--- pytaurus/__init__.py
+++ pytaurus/__init__.py
@@ -13,7 +13,7 @@ Folder_Data_Potential = 'Potential'
 Folder_Data_Substrate = sen.Folder_Substrate
 Folder_Data_Trap = 'Trap'
 
-Files_Remain = [sen.User_Param_File, sen.TimeStep_File, sen.Trapped_In_File]
+Files_Remain = [sen.User_Param_File, sen.TimeStep_File, sen.Trapped_In_File, sen.Subs_Data_File]
 
 Folers_In_Projects = [Folder_Data_Band, Folder_Data_Current, Folder_Data_Density, Folder_Data_ElecField,
                       Folder_Data_Potential, Folder_Data_Substrate, Folder_Data_Trap,
